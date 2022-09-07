1a89eb4fe01868aa623693e098b253d9b78c416a
aminghorbankhani <aminghorbankhani.ir@gmail.com>
2022-09-07 13:16:54 +0430

    fix user image size

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -5,7 +5,7 @@ import { User } from '../types';
 const UserItem = ({ user }: { user: User }): JSX.Element => (
   <li key={user.id} className="border-b last:border-b-0">
     <Link to={`/${user.id}`} className="p-5 flex flex-row items-center">
-      <img src={user.image} alt={user.username} className="w-16 aspect-auto rounded-full bg-indigo-300" />
+      <img src={user.image} alt={user.username} className="w-16 h-16 rounded-full bg-indigo-300" />
       <h3 className="ml-5">{user.firstName} {user.lastName}</h3>
     </Link>
   </li>
